5ab7362e04d803257354a46ac3d9aa02fbd0322c
Tristan Matthias <tristanmatthias94@gmail.com>
2014-04-02 17:10:32 +1000

    Fixed issue with index route

--- dist/routes/index.js
+++ dist/routes/index.js
@@ -10,15 +10,9 @@ var index = function(config) {
     cachebust: cachebust
   };
 
-  // In the event plain html pages are being used, need to
-  // switch to different page for optimized view
-  var name = "index";
-  if (config.isOptimize && config.server.views.html) {
-    name += "-optimize";
-  }
 
   return function(req, res) {
-    res.render(name, options);
+    res.render("index", options);
   };
 };
 
