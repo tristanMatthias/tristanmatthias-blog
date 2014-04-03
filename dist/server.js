56f8b6ce9d5235db607679469f5ffaad6416f047
Tristan Matthias <tristanmatthias94@gmail.com>
2014-04-03 12:22:27 +1000

    Build

--- dist/server.js
+++ dist/server.js
@@ -30,8 +30,29 @@ exports.startServer = function(config, callback) {
     app.use(express.errorHandler());
   });
 
+  
   app.get('/', routes.index(config));
-  app.get('/contact', routes.contact());
+  app.post('/contact', routes.contact());
+
+  app.use(function(req, res, next){
+    res.status(404);
+
+    // respond with html page
+    if (req.accepts('html')) {
+      res.render('404', { url: req.url });
+      return;
+    }
+
+    // respond with json
+    if (req.accepts('json')) {
+      res.send({ error: 'Not found' });
+      return;
+    }
+
+    // default to plain-text. send()
+    res.type('txt').send('Not found');
+  });
+
 
   callback(server);
 };
