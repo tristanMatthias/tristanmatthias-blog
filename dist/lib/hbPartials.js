30a74a7b0c420c5eb34a592c5ff5946c69c28aa6
Tristan Matthias <tristanmatthias94@gmail.com>
2014-04-08 17:05:00 +1000

    Added

---
+++ dist/lib/hbPartials.js
@@ -0,0 +1,16 @@
+var hbs = require('handlebars');
+var fs = require('fs');
+
+var partialsDir = './views/partials';
+
+var filenames = fs.readdirSync(partialsDir);
+
+filenames.forEach(function (filename) {
+  var matches = /^([^.]+).hbs$/.exec(filename);
+  if (!matches) {
+    return;
+  }
+  var name = matches[1];
+  var template = fs.readFileSync(partialsDir + '/' + filename, 'utf8');
+  hbs.registerPartial(name, template);
+});
